46feab38e87b38d116602e01ad12103016ab4ac5
Jiawen Zhuo <jiawen.zhuo@uq.net.au>
2022-08-10 18:53:08 +1000

    define globalstyle

---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle, withTheme } from "styled-components";
+import { ThemeProps } from "./themes";
+
+const globalStyle = createGlobalStyle`
+    :root{
+        //dark mode
+        --dark--background: #2B2C37;
+        --dark-text:#FFFFFF;
+
+        //light mode
+        --light--background:#F2F2F2;
+        --light-text:#000112;
+
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline:0;
+    }
+
+    body{
+        //styleName: Body (L);
+        font-family: Plus Jakarta Sans;
+        font-size: 13px;
+        font-weight: 500;
+        line-height: 23px;
+        letter-spacing: 0px;
+        text-align: left;
+    }
+
+    //Heading(XL)
+    h1{
+        //styleName: Heading (XL);
+        font-family: Plus Jakarta Sans;
+        font-size: 24px;
+        font-weight: 700;
+        line-height: 30px;
+        letter-spacing: 0px;
+        text-align: left;
+    }
+`;
---
+++ src/styles/themes.ts
@@ -0,0 +1,9 @@
+export interface ThemeProps {
+  background: string;
+  text: string;
+}
+
+export const darkTheme: ThemeProps = {
+  background: "var(--dark-background)",
+  text: "var(--dark-text)",
+};
