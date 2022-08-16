5df5067c20e61bcb3c193a72d30378766f622872
Jiawen Zhuo <jiawen.zhuo@uq.net.au>
2022-08-16 22:55:32 +1000

    add icons

--- icons/index.ts
+++ icons/index.ts
@@ -1,5 +1,7 @@
 export { default as Board } from "./Board";
+export { default as Eye } from "./Eye";
 export { default as EyeSlash } from "./EyeSlash";
+export { default as Kanban } from "./Kanban";
 export { default as KanbanLogo } from "./KanbanLogo";
 export { default as Moon } from "./Moon";
 export { default as Sun } from "./Sun";
